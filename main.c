72de1e1d4dad129f5920b38e0863387990bc80ac
Marko Varga <markosvarga@gmail.com>
2021-02-04 00:30:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Name: thr_atomic.c
+
+int main(int argc, char *argv[]) {
+  printf("The early bird gets the worm!\n");
+  int m;
+  int n;
+  
+
+  //printf("Enter m\n");
+  scanf("%d", &m);
+  //printf("Enter n\n");
+  scanf("%d", &n);
+
+  printf( "\nYou entered: m = %d, n = %d ", m, n);
+
+  return 0;
+}
