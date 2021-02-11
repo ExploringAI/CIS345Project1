f6c915b274bfded66b43b334ba54e5569a34d127
Marko Varga <markosvarga@gmail.com>
2021-02-11 02:40:56 +0000

    This sh**t works?! <-- NOT

--- main.c
+++ main.c
@@ -4,31 +4,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 
-/**
+double global_sum = 0; // setting the sum of both to 0
+
+/** TODO:
 Each thread computes the
 sum of n/m quadruple roots. Namely, the first thread (i.e. thread 0) computes the sum of quadruple roots from
 1 to n/m, the second thread (i.e. thread 1) computes the sum of the quadruple roots from n/m+1 to 2n/m, etc.
 When a thread finishes its computation, it should print its partial sum and atomically add it to a shared global
 variable. 
 */
+/**
 void *thread(void *arg) {
   char *ret;
 
 
-  printf("thread() entered with argument '%s' '%s'\n", arg[1], arg[2]);
+  //printf("thread() entered with argument '%s' '%s'\n", arg[1], arg[2]);
   if ((ret = (char*) malloc(20)) == NULL) {
     perror("malloc() error");
     exit(2);
   }
   strcpy(ret, "This is a test");
   pthread_exit(ret);
+}*/
+
+void * collector(void* arg)
+{
+    int* a = (int*)arg;
+    double quadrupleRoot = .25;
+    double result[1];
+    result[0] = pow((double)a[0]/(double)a[1], quadrupleRoot);
+    printf("m=%d, n=%d, & m/n = %f: so quadruple root of m/n = %f\n", a[0], a[1], (double)a[0]/(double)a[1], result[0]);
+    pthread_exit((void*)(result));
 }
 
 
-
-
+// to use the Shell in Repl.it command is: ./main "m" "n"
+//where m and n are integers
 int main(int argc, char *argv[]) {
   printf("The early bird gets the worm!\n");
 
@@ -40,10 +54,12 @@ int main(int argc, char *argv[]) {
   sscanf(argv[2], "%d", &n);
   printf( "\nYou entered: m = %d, n = %d \n\n", m, n);
 
-  /**Next, it creates m threads using pthread create() and each thread 
+  /**TODO: Next, it creates m threads using pthread create() and each thread 
   computes the sum of n/m quadruple roots.*/
+  /**
   pthread_t thid;
   void *ret;
+  //creates instance of params structure
 
   if (pthread_create(&thid, NULL, thread, argv) != 0) {
     perror("pthread_create() error");
@@ -56,7 +72,23 @@ int main(int argc, char *argv[]) {
   }
 
   printf("thread exited with '%s'\n", ret);
+*/
+  int i, mn[2];
+  mn[0] = m;
+  mn[1] = n;
+  pthread_t thread_tid[m];
+  void* passResult;
+  double result;
 
+  for(i = 0; i < m; i++) {
+      pthread_create(&thread_tid[i], NULL, collector, (void*)(mn));
+  }
+
+  for(i = 0; i < m; i++) {
+      pthread_join(thread_tid[i], &passResult);
+      result = ((double*)passResult)[0];
+      printf( "\nThis sh**t works?!: result = %f\n\n",  result);
+  }
 
 
   return 0;
